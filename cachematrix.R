c542eafe2def878494d3a7e8c28a8f92ff491057
Younghyun Chung <younghyun.chung@gmail.com>
2014-05-21 11:11:36 -0400

    added overal description

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,15 @@
-## Put comments here that give an overall description of what your
-## functions do
+## cachematrix saves time and resource of matrix invert calculation 
+## by caching the invert of matrix. To achieve this, it creates a 
+## special matrix that stores matrix values and its invert. 
 
-## Creates a matrix object that can cache its inverse matrix.
+## makeCacheMatrix: Creates a matrix object that can cache its inverse matrix.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Computes the inverse of the matrix returned by makeCacheMatrix().
+## cacheSolve: Computes the inverse of the matrix returned by makeCacheMatrix().
 ## If the inverse has already been calculated and the matrix has not changed,
 ## then it retrieve the inverse from cache.
 
