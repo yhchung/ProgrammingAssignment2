fd4fc46eb78e4536a7add650919cdf89e52f6138
Younghyun Chung <younghyun.chung@gmail.com>
2014-05-21 11:01:37 -0400

    added function description
    function description of makeCache Matrix and cache Solve

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,16 @@
 ## Put comments here that give an overall description of what your
 ## functions do
 
-## Write a short comment describing this function
+## Creates a matrix object that can cache its inverse matrix
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## Computes the inverse of the matrix returned by makeCacheMatrix().
+## If the inverse has already been calculated and the matrix has not changed,
+## then it retrieve the inverse from cache.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
